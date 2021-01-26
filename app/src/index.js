687ec970774cdf0bfd2d990c0e4086eca5dc5c42
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2021-01-25 21:35:30 -0500

    update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import * as serviceWorker from './serviceWorker';
+import { contactsReducer } from './reducers';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+const addressBookApp = combineReducers({
+    contacts: contactsReducer,
+})
+const store = createStore(addressBookApp)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
